72dba52633b771ab811593f90378014711cedc18
Hugo L. Ruffo <hlruffo@gmail.com>
2024-06-23 09:39:45 -0300

    first commit

---
+++ ProdutosApp.Domain/Dtos/ProdutosLogDto.cs
@@ -0,0 +1,17 @@
+﻿namespace ProdutosApp.Domain.Dtos
+{
+    public class ProdutosLogDto
+    {
+        public Guid Id { get; set; }
+        public DateTime? DataHora { get; set; }
+        public TipoLog? Tipo { get; set; }
+        public ProdutosResponseDto? Produto { get; set; }
+    }
+
+    public enum TipoLog
+    {
+        CREATE = 1,
+        UPDATE = 2,
+        DELETE = 3
+    }
+}
---
+++ ProdutosApp.Domain/Mappings/ProfileMap.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using ProdutosApp.Domain.Dtos;
+using ProdutosApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ProdutosApp.Domain.Mappings
+{
+    public class ProfileMap : Profile
+    {
+        public ProfileMap()
+        {
+            CreateMap<Categoria, CategoriasResponseDto>();
+
+            CreateMap<ProdutosRequestDto, Produto>();
+
+            CreateMap<Produto, ProdutosResponseDto>()
+                .AfterMap((src, dest) =>
+                {
+                    dest.Total = src.Preco * src.Quantidade;
+                });
+        }
+    }
+}
+
+
+
